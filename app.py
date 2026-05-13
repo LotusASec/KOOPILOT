0360528474ee1d67f3ddf4050796db69c075de1d
ABilgin <128148196+LotusASec@users.noreply.github.com>
2026-05-13 21:01:21 +0300

    Configure app to run on specified host and port

--- app.py
+++ app.py
@@ -276,4 +276,8 @@ def ai_status():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
